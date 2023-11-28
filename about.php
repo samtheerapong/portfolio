4e5ee99807f2009ddb6a04a0b07229ad4f6b2c7c
samtheerapong <theerapong.khan@gmail.com>
2023-11-28 12:42:57 +0700

    up

--- about.php
+++ about.php
@@ -103,7 +103,7 @@
             <div class="col-lg-6">
 
                 <div class="progress">
-                    <span class="skill">HTML <i class="val">80%</i></span>
+                    <span class="skill">Web Application Developer <i class="val">80%</i></span>
                     <div class="progress-bar-wrap">
                         <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
                         </div>
@@ -111,7 +111,7 @@
                 </div>
 
                 <div class="progress">
-                    <span class="skill">CSS <i class="val">70%</i></span>
+                    <span class="skill">MRP / ERP <i class="val">70%</i></span>
                     <div class="progress-bar-wrap">
                         <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
                         </div>
@@ -119,9 +119,9 @@
                 </div>
 
                 <div class="progress">
-                    <span class="skill">PHP <i class="val">75%</i></span>
+                    <span class="skill">Hardware / Software <i class="val">90%</i></span>
                     <div class="progress-bar-wrap">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
                         </div>
                     </div>
                 </div>
